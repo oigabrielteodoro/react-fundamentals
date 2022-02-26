28084d1f05ea4b7735ff249f76346284dd9c202e
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-02-25 23:28:26 -0300

    feat: add prop-types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React from "react";
 
+import Post from "./Post";
+
 export default function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>JStack's Blog</h1>
+      <h2>Posts da semana</h2>
+      <hr />
+
+      <Post
+        likes={20}
+        post={{
+          title: "Título da notícia 01",
+          subtitle: "Subtítulo da notícia 01",
+        }}
+      />
+    </>
+  );
 }
---
+++ src/Post.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Post({ post: { title, subtitle }, likes }) {
+  return (
+    <article>
+      <strong>{title}</strong>
+      <br />
+      <small>{subtitle}</small>
+      <br />
+      Media: {likes / 2}
+    </article>
+  );
+}
+
+Post.propTypes = {
+  likes: PropTypes.number.isRequired,
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+  }).isRequired,
+};
